4eb9bc8607d7b8954bf28f8bc6324673aa469eae
Omarja21 <jarrahom21@gmail.com>
2021-02-28 21:17:14 +0200

    This is Lab 01b

---
+++ Reading_201/Lab_01b/App.js
@@ -0,0 +1,11 @@
+let FirstQuestion=prompt('What was your first pet name?');
+let SecondQuestion=prompt('whom do you love more your mom or your dad?')
+let ThirdQuestion=prompt('what is your favourite football team?')
+let FourthQuestion=prompt('how was you day pet lover?')
+
+alert(FirstQuestion)
+alert(SecondQuestion)
+alert(ThirdQuestion)
+alert(FourthQuestion)
+alert( FirstQuestion +'   '+ SecondQuestion +'   '+ThirdQuestion+'   '+ FourthQuestion)
+console.log(FirstQuestion + SecondQuestion + ThirdQuestion + FourthQuestion)
